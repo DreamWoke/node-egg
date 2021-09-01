b3eac9294672ca0d939f73714c0d49b9278b1773
yaojun qiu <851942730@qq.com>
2021-09-01 16:19:04 +0800

    删除腾讯云 key

--- app/utils/cos.js
+++ app/utils/cos.js
@@ -2,8 +2,7 @@ const COS = require('cos-nodejs-sdk-v5');
 
 const cosUtil = {
   cos: new COS({
-    SecretId: 'AKIDD9HApFpLqRpyfXf2eLo2ZApFUUUPsxts', // 密钥id
-    SecretKey: 'tY04HJk1OVGEvWC6k7IQVKsyNclscHfn', // 密钥key
+
   }),
   Bucket: 'console-upload-1300990907', // 存储桶名称
   Region: 'ap-shanghai', // 存储桶区域
